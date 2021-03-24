1bd679e335f4e95c0755e981f959b0e0d0b42100
bXr1 <77245205+bXr1@users.noreply.github.com>
2021-03-23 20:37:10 -0400

    first project image

---
+++ src/test/java/practice/ComplexActionTest.java
@@ -0,0 +1,12 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class ComplexActionTest {
+    @Test
+    public void complex (){
+
+        System.out.println("hey");
+    }
+}
+
